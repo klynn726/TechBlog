7b0fadabdd55f3ed0d6c229180611a4e1d318b98
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-05-08 10:45:05 -0700

    added examples from module to config controllers and models files

---
+++ controllers/index.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+// below are examples from the module
+
+// const homeRoutes = require('./home-routes.js');
+// const dashboardRoutes = require('./dashboard-routes.js');
+
+// router.use('/dashboard', dashboardRoutes);
+// router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
