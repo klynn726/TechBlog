e51f5c7fe6c0c317aed35659d9521349aa2d39a8
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-05-11 16:58:43 -0700

    but fix secret

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: '',
+  secret: 'Secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
